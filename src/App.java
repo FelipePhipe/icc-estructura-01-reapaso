980a149788ac123916966c859896af7dbebed4ae
FelipePhipe <felipeparragranda@gmail.com>
2025-04-22 19:43:27 -0500

    Repaso

--- src/App.java
+++ src/App.java
@@ -1,14 +1,38 @@
 
+import Controllers.PersonaController;
 import Models.Persona;
+import views.ViewConsole;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Persona[] personas = new Persona[] {
-
+        Persona[] personas = new Persona[] { 
+            new Persona("Juan",25),
+            new Persona("Maria",30),
+            new Persona("Carlos",22),
+            new Persona("Ana",28),
+            new Persona("Luis",35),
+            new Persona("Sofia",27),
+            new Persona("Miguel",40),
+            new Persona("Laura",32),
+            new Persona("Pedro",29),
+            new Persona("Elena",26),
+    
         };
 
-        /// TODO: GENERAR INSTNACIA DE CONTROLLE Y ORDENAR EL METODO DESPUES BUSCAR POR
-        /// LA EDAD
+        PersonaController pC= new PersonaController();
+        ViewConsole vC= new ViewConsole();
+
+        vC.printMessage("Arreglo personas");
+        vC.prinrArrayPersons(personas);
+
+        vC.printMessage("Arreglo Ordeado por insercion");
+
+        pC.ordenarPorEdad(personas);
+        vC.prinrArrayPersons(personas);
 
     }
 }
+        
+
+
+
---
+++ src/views/ViewConsole.java
@@ -0,0 +1,15 @@
+package views;
+
+import Models.Persona;
+
+public class ViewConsole {
+    public void prinrArrayPersons(Persona[] personas){
+
+
+    }
+
+    public void printMessage(String message){
+
+    }
+    
+}
